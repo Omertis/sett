255a6c86c14182136f59fd11cd64c4014cb77b7f
Omertis <36013948+Omertis@users.noreply.github.com>
2018-05-22 22:31:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('guildMemberAdd', member => {
   if (member.user.bot) return;
   var Canvas = require('canvas')
   var jimp = require('jimp')
-  const w = ['./img/bull1i.png'];
+  const w = ['./img/bull1.png'];
           let Image = Canvas.Image,
               canvas = new Canvas(749, 198),
               ctx = canvas.getContext('2d');
